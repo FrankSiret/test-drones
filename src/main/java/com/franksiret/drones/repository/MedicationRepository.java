4445195892330ae836b7600113b790a552b8dea7
FrankSiret <frank.siret@email.com>
2023-02-25 00:38:46 -0500

    add request to get all load medications of a drone

--- src/main/java/com/franksiret/drones/repository/MedicationRepository.java
+++ src/main/java/com/franksiret/drones/repository/MedicationRepository.java
@@ -1,6 +1,7 @@
 package com.franksiret.drones.repository;
 
 import com.franksiret.drones.domain.Medication;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface MedicationRepository extends JpaRepository<Medication, Long>, JpaSpecificationExecutor<Medication> {}
+public interface MedicationRepository extends JpaRepository<Medication, Long>, JpaSpecificationExecutor<Medication> {
+    List<Medication> findAllByDroneId(Long id);
+}
